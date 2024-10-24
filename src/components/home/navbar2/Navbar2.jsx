80e95d9194f2abe0db16cd1ec20fa09676e0533e
Chirag <chiragsoni8857@gmail.com>
2024-10-24 10:51:52 +0530

    helloo

--- src/components/home/navbar2/Navbar2.jsx
+++ src/components/home/navbar2/Navbar2.jsx
@@ -4,6 +4,8 @@ import hospital from '../images/hospital.png';
 import email from '../images/email.png';
 import phone from '../images/phone.png';
 import complain from '../images/complain.jpg';
+// import login from '../images/login.png'
+// import {Link} from 'react-router-dom'
 // import {Link} from 'react-router-dom'
 
 const Navbar2 = () => {
@@ -13,10 +15,13 @@ const Navbar2 = () => {
         <div><img className='navbar211img' src={hospital} alt=''></img></div>
       </div>
       <div className='navbar22'>
-        <div className='navbar221'><img className='navbar221img' src={email} alt=''></img>karancpppython@gmail.com</div>
+        <div className='navbar221'><img className='navbar221img' src={email} alt=''></img>hospital.support@gmail.com</div>
         <div className='navbar221'><img className='navbar222img' src={phone} alt=''></img>9832826152</div>
         <div className='navbar221'><img className='navbar222img' src={complain} alt=''></img>Complaint</div>
       </div>
+      {/* <div className='n12'>
+        <div className='hnav2'><Link to='/login'><img className='himg' src={login} alt='login'></img></Link></div>
+      </div> */}
     </div>
   )
 }
